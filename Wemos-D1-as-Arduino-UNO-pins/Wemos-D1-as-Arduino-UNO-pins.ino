5b3935a3d3f398764b8644065b3ef8a3c9ee099c
alex.anipchenko <alex.anipchenko@gmail.com>
2020-01-08 18:20:40 +0200

    define pins as arduino uno

--- Wemos-D1-as-Arduino-UNO-pins/Wemos-D1-as-Arduino-UNO-pins.ino
+++ Wemos-D1-as-Arduino-UNO-pins/Wemos-D1-as-Arduino-UNO-pins.ino
@@ -1,9 +1,25 @@
-void setup() {
-  // put your setup code here, to run once:
+//link D-pins to GPIO for version R1
+#define D0 3
+#define D1 1
+#define D2 16
+#define D3 5
+#define D4 4
+#define D5 14
+#define D6 12
+#define D7 13
+#define D8 0
+#define D9 2
+#define D10 15
 
+//link A-pin
+#define A0 0
+
+void setup() 
+{
+  
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop() 
+{
+  
 }
