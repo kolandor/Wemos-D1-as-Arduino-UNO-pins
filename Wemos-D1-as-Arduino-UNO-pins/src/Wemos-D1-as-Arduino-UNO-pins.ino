97f2a7744ca3e1cb8d82a6e58a78beb5e71e5248
alex.anipchenko <alex.anipchenko@gmail.com>
2020-01-10 16:15:04 +0200

    Add more information defines

--- Wemos-D1-as-Arduino-UNO-pins/src/Wemos-D1-as-Arduino-UNO-pins.ino
+++ Wemos-D1-as-Arduino-UNO-pins/src/Wemos-D1-as-Arduino-UNO-pins.ino
@@ -4,17 +4,17 @@
 void setup() 
 {
   // initialize LED digital pin as an output.
-  pinMode(D9, OUTPUT);
+  pinMode(BOARD_LED, OUTPUT);
 }
 
 void loop() 
 {
   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(D9, HIGH);
+  digitalWrite(BOARD_LED, HIGH);
   // wait for a second
   delay(1000);
   // turn the LED off by making the voltage LOW
-  digitalWrite(D9, LOW);
+  digitalWrite(BOARD_LED, LOW);
    // wait for a second
   delay(1000);
 }
