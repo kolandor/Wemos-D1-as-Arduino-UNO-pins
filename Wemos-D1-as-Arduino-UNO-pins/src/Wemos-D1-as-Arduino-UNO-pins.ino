006133e35dc892492a9ab208879856e3856e4eec
alex.anipchenko <alex.anipchenko@gmail.com>
2020-01-10 14:47:18 +0200

    Add blink template using WemosD1R1 lib

--- Wemos-D1-as-Arduino-UNO-pins/src/Wemos-D1-as-Arduino-UNO-pins.ino
+++ Wemos-D1-as-Arduino-UNO-pins/src/Wemos-D1-as-Arduino-UNO-pins.ino
@@ -1,10 +1,20 @@
+#include <Arduino.h>
+#include <WemosD1R1Pins.h>
 
 void setup() 
 {
-  
+  // initialize LED digital pin as an output.
+  pinMode(D9, OUTPUT);
 }
 
 void loop() 
 {
-  
+  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(D9, HIGH);
+  // wait for a second
+  delay(1000);
+  // turn the LED off by making the voltage LOW
+  digitalWrite(D9, LOW);
+   // wait for a second
+  delay(1000);
 }
